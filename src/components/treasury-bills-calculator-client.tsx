0df27f4a883584984ee647fb3487477e8d2b647d
dsaad12344321-svg <dsaad12344321@gmail.com>
2026-04-18 16:53:49 +0200

    Update treasury-bills-calculator-client.tsx

--- src/components/treasury-bills-calculator-client.tsx
+++ src/components/treasury-bills-calculator-client.tsx
@@ -117,7 +117,7 @@ export default function TreasuryBillsCalculatorClient() {
           </div>
             <div className="flex justify-between">
             <span>مدة أذون الخزانة بالأيام</span>
-            <strong> يوم {DAYS_MAP[duration]} </strong>
+            <strong>{DAYS_MAP[duration]}يوم</strong>
           </div>
           <div className="flex justify-between">
             <span>القيمة الحالية للجنيه</span>

5e8b382a2d0f145ce8a32dedd314e06a9a64eb0b
dsaad12344321-svg <dsaad12344321@gmail.com>
2026-08-27 13:48:49 +0300

    Update deposits-poster-client.tsx

--- src/components/deposits-poster-client.tsx
+++ src/components/deposits-poster-client.tsx
@@ -198,17 +198,17 @@ const selectedDeposits =
       const node = posterRef.current;
 
       const dataUrl = await toPng(node, {
-        pixelRatio: 4,
+        pixelRatio: 2,
         cacheBust: true,
 
         width: node.scrollWidth,
         height: node.scrollHeight,
 
         canvasWidth:
-          node.scrollWidth * 4,
+          node.scrollWidth * 2,
 
         canvasHeight:
-          node.scrollHeight * 4,
+          node.scrollHeight * 2,
 
         style: {
           margin: "0",
